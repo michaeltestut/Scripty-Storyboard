6b3934e7faf62d6d7b07efba9d7ca339011e0d64
Michael Testut <michael.testut@yahoo.com>
2023-05-30 21:19:05 -0700

    start project

---
+++ Scripty-Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scripty-Storyboard
+//
+//  Created by Michael Testut on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
